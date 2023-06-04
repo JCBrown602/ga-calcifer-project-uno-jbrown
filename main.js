91c3f120642b44278049460baf7c8bf879425901
jcbrown602 <3117301+JCBrown602@users.noreply.github.com>
2023-06-04 16:37:45 -0500

    Start adding framework.

---
+++ main.js
@@ -0,0 +1,13 @@
+
+// (MODEL)
+
+// (CONTROLLER)
+function init() {
+    console.log("Init...");
+    render();
+};
+
+// (VIEW) All DOM action takes place here
+function render() {
+    console.log("Render...");
+};
