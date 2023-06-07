e7339787360cc429315c579482b49de564361e0a
jcbrown602 <3117301+JCBrown602@users.noreply.github.com>
2023-06-07 11:23:06 -0500

    Begin deal function.
    Deal function deals 1 card.

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 // Build deck: 52 cards, 13 face values, 4 suits
 const suits = ['Clubs','Diamonds','Hearts','Spades'];
 const faces = 
-    ['A','2','3','4','5','6','7','8','9','10','Jack','Queen','King'];
+    ['Ace','2','3','4','5','6','7','8','9','10','Jack','Queen','King'];
 let deck = [];
 faces.forEach((face) => {
     suits.forEach((suit) => {
@@ -15,26 +15,26 @@ faces.forEach((face) => {
 });
 
 // Players: Player, Computer1, Computer2, etc.
-let player = [
+let players = [
     {
         "name": "Player",
         "score": 0,
-        "hand": []  // What cards is this player holding
+        "hand": [{face: 'Ace', suit: 'Spades'}, {face: 'Ace', suit: 'Hearts'}]  // What cards is this player holding
     },
     {
         "name": "Computer1",
         "score": 0,
-        "hand": []
+        "hand": [{face: 'Ace', suit: 'Spades'}, {face: 'Ace', suit: 'Hearts'}]
     },
     {
         "name": "Computer2",
         "score": 0,
-        "hand": []
+        "hand": [{face: 'Ace', suit: 'Spades'}, {face: 'Ace', suit: 'Hearts'}]
     },
     {
         "name": "Computer3",
         "score": 0,
-        "hand": []
+        "hand": [{face: 'Ace', suit: 'Spades'}, {face: 'Ace', suit: 'Hearts'}]
     }
 ]
 /*----- app's state (variables) -----*/
@@ -51,17 +51,25 @@ function render() {
 /*----- functions -----*/
 function init() {
     console.log("Init...");
+    // Players have zero cards
     console.log("The starting deck:");
     console.log(deck.slice(0,5));
+    // Shuffle cards
     shuffle(deck);
     console.log(deck.slice(0,5));
+    // Deal cards (first two)
+    console.log("The players currently have:");
+    showPlayerHands();
+    dealCards();
+    showPlayerHands();
+    // Betting rounds
     render();
 };
 
 // Dealer?
 
 // Shuffle deck
-function shuffle(deck){
+function shuffle(){
     console.log("Shuffling...");
     for(let i = 0; i < 52; i ++) {
         let tempCard = deck[i];
@@ -72,11 +80,32 @@ function shuffle(deck){
     return deck;
 }
 // Deal cards
+function dealCards() {
+    console.log("Dealing...");
+    for(let i = 0; i < players.length; i++) {
+        let cardToDeal = deck.pop();
+        console.log(`${players[i].name} 
+            - ${players[i].hand.length} - ${cardToDeal.face} of ${cardToDeal.suit}`);
+        players[i].hand.push(cardToDeal);
+        console.log(players[i]);
+    }
+    console.log(players);
+    return players;
+}
 
 // Betting
 
 // Scorekeeper
 
-//console.log(deck);
+function showPlayerHands() {
+    // Loop through any number of players
+    players.forEach((player) => {
+        console.log(`${player.name} has `);
+        // Loop through any number of cards
+        player.hand.forEach((card) => {
+            console.log(`\t${card.face} of ${card.suit}`)
+        });
+    });
+}
 
 init();
